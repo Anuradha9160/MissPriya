aae2bb597b8ba816e5910b299079371d3fe65964
Anuradha9160 <105846530+Anuradha9160@users.noreply.github.com>
2026-04-03 08:31:25 +0530

    Update config_sample.py

--- config_sample.py
+++ config_sample.py
@@ -1,9 +1,9 @@
 # REQUIRED CONFIG
-BOT_TOKEN = ""
-OWNER_ID = 0
-TELEGRAM_API = 0
-TELEGRAM_HASH = ""
-DATABASE_URL = ""
+BOT_TOKEN = "7846363502:AAHCoHoKRMQBmI0c9Q0Se3G3zYXz3PAnCWU"
+OWNER_ID = 2080060354
+TELEGRAM_API = 8864773
+TELEGRAM_HASH = "0d2355c5f0d713381fa9b47d76b4611e"
+DATABASE_URL = "mongodb+srv://Donvijay:Donvijay@cluster0.kjcuehv.mongodb.net/?appName=Cluster0"
 
 # OPTIONAL CONFIG
 DEFAULT_LANG = "en"
@@ -150,8 +150,8 @@ USENET_SERVERS = [
 ]
 
 # Update
-UPSTREAM_REPO = ""
-UPSTREAM_BRANCH = "master"
+UPSTREAM_REPO = "https://github.com/Anuradha9160/MissPriya"
+UPSTREAM_BRANCH = "wzv3"
 UPDATE_PKGS = True
 
 # Leech
